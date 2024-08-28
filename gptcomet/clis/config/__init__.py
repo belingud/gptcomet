5a4a7b8abb2bc29645a985c76ba7777eebeb9726
belingud <1170202353@qq.com>
2024-08-28 22:02:36 +0800

    feat: Add path command to config management

--- gptcomet/clis/config/__init__.py
+++ gptcomet/clis/config/__init__.py
@@ -1,6 +1,6 @@
 import typer
 
-from gptcomet.clis.config import get, reset, set
+from gptcomet.clis.config import get, path, reset, set
 from gptcomet.clis.config import list as _list
 from gptcomet.utils import CONTEXT_SETTINGS
 
@@ -12,3 +12,4 @@ app.command(name="get")(get.entry)
 app.command(name="list")(_list.entry)
 app.command(name="reset")(reset.entry)
 app.command(name="set")(set.entry)
+app.command(name="path")(path.entry)
---
+++ gptcomet/clis/config/path.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+
+import typer
+
+from gptcomet.config_manager import ConfigManager, get_config_manager
+from gptcomet.utils import console
+
+
+def entry(
+    local: Annotated[bool, typer.Option(
+        "--local", help="Use local configuration file.", rich_help_panel="Options"
+    )] = False,
+):
+    cfg: ConfigManager = get_config_manager(local=local)
+    console.print(cfg.current_config_path)
