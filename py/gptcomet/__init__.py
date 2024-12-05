80f9e59cb3fdaa920fffefdff4e8d42b8fdf626f
belingud <1170202353@qq.com>
2024-12-05 22:19:35 +0800

    chore: optimize wheel building process

--- py/gptcomet/__init__.py
+++ py/gptcomet/__init__.py
@@ -1,5 +1,18 @@
 """
 gptcomet Python package
+
+GPTComet is an AI-powered Git commit message generator that leverages
+natural language processing to create meaningful and descriptive commit
+messages based on code changes. It aims to improve the quality and
+consistency of commit messages in software development projects.
+
+Key features:
+- Automatic generation of commit messages
+- Support for multiple languages
+- Customizable configuration
+- Integration with Git workflows
+
+For more information, visit: https://github.com/belingud/gptcomet
 """
 
-__version__ = "0.1.0"  # This should match your Go version
+__version__ = "0.1.0"
